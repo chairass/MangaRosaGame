ae454b71b9997adc424b20b0646972256411c571
chairass <chairassacramento@gmail.com>
2025-02-27 23:10:56 -0300

    .

--- MangaRosaMemoryGame.java
+++ MangaRosaMemoryGame.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
 public class MangaRosaMemoryGame {
+
+    /// declarei as variaveis
     static Scanner scanner = new Scanner(System.in);
     private static int pontuacaoJ1 = 0;
     private static int pontuacaoJ2 = 0;
@@ -10,60 +11,59 @@ public class MangaRosaMemoryGame {
     private static String jogador1 = "PARTICIPANTE01";
     private static String jogador2 = "PARTICIPANTE02";
     private static String tamTabuleiro;
+    private static int coluna;
+    private static int linha;
 
+    /// cores
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[41m";
+    public static final String BLUE = "\u001B[44m";
 
     public static void main(String[] args) {
-        System.out.println("\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550");
-        System.out.println("Manga Rosa Memory Game");
-        System.out.println("\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550");
+        System.out.println("\u2550".repeat(22));        System.out.println("Manga Rosa Memory Game");
+        System.out.println("\u2550".repeat(22));
 
-        String[][] matrizTabuleiro = new String[4][4];
-
-
-
-        ArrayList<String> tabuleiro1 = new ArrayList<>();
-
-
-
-        while (resposta <= 3){
+        while (resposta <= 3) {
             System.out.println("1. Iniciar\n2. Pontuação participantes\n3. Regras do Jogo\n4. Sair");
             resposta = scanner.nextInt();
-            if (resposta == 1){
+            if (resposta == 1) {
+                System.out.println("\u2550".repeat(22));
                 System.out.println("Para começar o jogo informe o nome dos participantes");
                 System.out.print("Jogador 1: ");
                 jogador1 = scanner.next();
                 System.out.print("Jogador 2: ");
                 jogador2 = scanner.next();
 
+                System.out.println("\u2550".repeat(22));
                 System.out.println("Informe o tamanho do tabuleiro que voce deseja:\nA: 4x4\nB: " +
-                        "6x6\nC: 8x8 \nD: 10x10" );
+                        "6x6\nC: 8x8 \nD: 10x10");
                 tamTabuleiro = scanner.next();
 
-                if (tamTabuleiro.equalsIgnoreCase("A")){
-
+                if (tamTabuleiro.equalsIgnoreCase("A")) {
+                    gerarTabuleiro();
+                    System.out.println("Digite a posição da primeira carta que deseja revelar");
                 }
-                else if (tamTabuleiro.equalsIgnoreCase("B")){
+                else if (tamTabuleiro.equalsIgnoreCase("B")) {
                     System.out.println("6x6");
                 }
-                else if (tamTabuleiro.equalsIgnoreCase("C")){
+                else if (tamTabuleiro.equalsIgnoreCase("C")) {
                     System.out.println("8x8");
                 }
-                else if (tamTabuleiro.equalsIgnoreCase("D")){
+                else if (tamTabuleiro.equalsIgnoreCase("D")) {
                     System.out.println("10x10");
                 }
             }
 
-            if (resposta == 2){
-                if (pontuacaoJ1 == 0 && pontuacaoJ2 == 0){
+            if (resposta == 2) {
+                if (pontuacaoJ1 == 0 && pontuacaoJ2 == 0) {
                     System.out.println("Ainda não há pontuação");
-                }
-                else {
+                } else {
                     System.out.println("O jogador" + jogador1 + "tem " + pontuacaoJ1 + "pontos " +
                             "O jogador" + jogador2 +
                             " tem " + pontuacaoJ2 + "pontos ");
                 }
             }
-            if (resposta == 3){
+            if (resposta == 3) {
                 System.out.println("Aqui vai as regras do jogo");
                 System.out.println("Nomes dos jogadores: Cada jogador deve ter um nome. Se não for informado, os nomes padrão serão \"PARTICIPANTE01\" e \"PARTICIPANTE02\".\n" +
                         "Pontuação:\n" +
@@ -83,5 +83,50 @@ public class MangaRosaMemoryGame {
             }
         }
     }
+
+    public static void gerarTabuleiro() {
+
+        ArrayList<String> tabuleiro = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                tabuleiro.add("F");
+            }
+        }
+
+        String[][] tabuleiro1 = new String[4][4];
+
+        int index = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                tabuleiro1[i][j] = tabuleiro.get(index);
+                index++;
+            }
+        }
+    }
+    public static void exibirTabuleiro(){
+        System.out.println("\u2550".repeat(22));
+        System.out.println("Que comece os jogos !!!");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void jogo(){
+
+        System.out.print("Digite a posição da primeira carta que deseja revelar\nLinha: ");
+        linha = scanner.nextInt();
+        System.out.println("Coluna");
+        coluna = scanner.nextInt();
+
+        if (linha >= 0 && linha < tabuleiro.length && coluna >= 0 && coluna < tabuleiro[0].length) {
+            System.out.println("Carta revelada: " + tabuleiro[linha][coluna]);
+        } else {
+            System.out.println("Posição inválida! Tente novamente.");
+        }
+
+    }
 }
+
 
